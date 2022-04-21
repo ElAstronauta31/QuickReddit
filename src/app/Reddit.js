b90db98de82b1f15e2abae2de6669d74c26bb93b
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-04-21 09:03:01 -0400

    Reddit api fetch file. Fetching a subreddit's posts and top subreddits

--- src/app/Reddit.js
+++ src/app/Reddit.js
@@ -1,1 +1,17 @@
+export const API_ROOT = 'https://www.reddit.com';
 
+export const getPosts = async (subreddit) => {
+    const respose = await fetch(`${API_ROOT}${subreddit}.json`);
+    const json = await respose.json();
+    const data = json.data.children.map((post) => post.data);
+
+    return data
+}
+
+export const getSubreddits = async () => {
+    const respose = await fetch(`${API_ROOT}/subreddits.json`);
+    const json = await respose.json();
+    const data = json.data.children.map((subreddit) => subreddit.data);
+    
+    return data
+}
