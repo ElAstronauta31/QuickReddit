8505d53af0047a407298a9e62f9e99337433ad34
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-05 19:29:11 -0400

    added reddit logo and website name to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,15 @@
+import React from "react";
+import { useDispatch } from "react-redux"; 
+import redditlogo from '../../sitecontent/reddit-logo.png';
 
+export const Header = () => {
+    const dispatch = useDispatch();
+    return(
+        <div>
+            <img src={redditlogo}></img>
+            <button>
+                QuickReddit
+            </button>
+        </div>
+    )
+}
