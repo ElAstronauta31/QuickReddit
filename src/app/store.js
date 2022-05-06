67b3b7de00ef4f4ad1c26ebc94b6c8ef6cd1e1ba
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-05 20:31:12 -0400

    added subreddit slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import subredditSlice from "../components/body/subreddit/subredditSlice";
 
 const store = configureStore({
     reducer: {
-
+        subreddits: subredditSlice
     }
 })
 
