1c2d6bfef7b02f03cf20e32ca41b8b37777251cf
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-05 20:27:24 -0400

    initial structure and created a slice, set inital state and added a reducer

--- src/components/body/subreddit/subredditSlice.js
+++ src/components/body/subreddit/subredditSlice.js
@@ -1,1 +1,18 @@
+import React from "react";
+import { createSlice } from '@reduxjs/toolkit';
 
+const subredditSlice = createSlice({
+    name: "subreddits",
+    initialState: {
+        subreddits: [],
+        activeSubreddit: "/r/meme/"
+    },
+    reducers: {
+        changeActiveSub: (state, action) => {
+            state.activeSubreddit = `/${action.payload}/`
+        }
+    }
+})
+
+export default subredditSlice.reducer;
+export const {changeActiveSub} = subredditSlice.actions;
