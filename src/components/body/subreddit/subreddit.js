567378154fde05c237d005eca6c3cc30cc5a208f
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-05 20:11:51 -0400

    initial subreddit component structure

--- src/components/body/subreddit/subreddit.js
+++ src/components/body/subreddit/subreddit.js
@@ -1,1 +1,14 @@
+import React from "react";
 
+export const Subreddit = () => {
+
+    return (
+        <div>
+            <h1>Top Subreddits</h1>
+            <ul>
+
+            </ul>
+            
+        </div>
+    )
+}
