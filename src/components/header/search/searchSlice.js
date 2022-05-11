4abe1881bf5fb7486d69481fee250e4640b9d85b
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-11 11:13:02 -0400

    created search slice and created setsearch reducer

--- src/components/header/search/searchSlice.js
+++ src/components/header/search/searchSlice.js
@@ -1,1 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+import React from "react";
 
+const searchSlice = createSlice({
+    name: "search",
+    initialState: '',
+    reducers: {
+        setSearch: (state, action) => (state = action.payload)
+    }
+})
+
+export const inputSelector = (state) => state.search;
+export default searchSlice.reducer;
+export const { setSearch } = searchSlice.actions;
