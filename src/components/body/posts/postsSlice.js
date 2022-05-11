1ce8e7149bfa8c0cb349d63cb108031bdee3e4dd
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-11 10:05:05 -0400

    created postslice using createSlice, set initial posts to empty array, used createAsyncThunk to prevent requesting data that has not been retrived yet, added loaidng states in extraReducers

--- src/components/body/posts/postsSlice.js
+++ src/components/body/posts/postsSlice.js
@@ -1,1 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import React from "react";
+import { getPosts } from "../../../app/Reddit";
 
+export const loadSubreddit = createAsyncThunk(
+    "subredditPost/loadPost",
+    async (activeSub) => {
+        const response = await getPosts(activeSub)
+        return response
+    }
+
+)
+
+
+const postsSlice = createSlice({
+    name: "posts",
+    initialState: {
+        posts: [],
+        isLoading: false
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(loadSubreddit.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(loadSubreddit.fulfilled, (state, action) => {
+            state.isloading = false
+            state.posts = action.payload
+        })
+        .addCase(loadSubreddit.rejected, (state) => {
+            state.isLoading = false
+        })
+    }
+})
+
+export default postsSlice.reducer;
