70f998686ebcd2c477c452d9ed8b7d800c7b5e89
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-11 11:54:07 -0400

    imported Post component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Header } from "./components/header/header";
 import { Subreddit } from "./components/body/subreddit/subreddit";
+import { Post } from "./components/body/posts/posts";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <main>
-        <Subreddit/>
-        {/* 
-        <Posts/> */}
+        <Subreddit/> 
+        <Post/>
       </main>
     </div>
   );
