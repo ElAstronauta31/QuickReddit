d4771d7574d8d26777ac6424733ef441e424ea80
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-11 11:26:10 -0400

    added selectors, dispatch and a filter for post when searching

--- src/components/body/posts/posts.js
+++ src/components/body/posts/posts.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { inputSelector } from "../../header/search/searchSlice";
+import { loadSubreddit } from "./postsSlice";
 
 export const Post = () => {
+    const activeSub = useSelector(state => state.subreddits.activeSubreddit);
+    const posts = useSelector(state => state.posts.posts);
+    const subreddits = useSelector(state => state.subreddits.subreddits);
+    const dispatch = useDispatch();
+    const searchTerm = useSelector(inputSelector);
+    const filteredPost = posts.filter((post) => post.title.toLowerCase().includes(searchTerm.toLowerCase()))
+
+
+    useEffect(() => {
+        dispatch(loadSubreddit(activeSub))
+    }, [dispatch, activeSub])
 
 
     return(
         <div>
             <div>
+                {
 
+                }
             </div>
         </div>
     )
