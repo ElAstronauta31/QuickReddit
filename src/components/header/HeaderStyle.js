c0356e0ccaabae4ded7b688f3997265e27b5692e
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-11 12:00:32 -0400

    added style using emotion/styled

--- src/components/header/HeaderStyle.js
+++ src/components/header/HeaderStyle.js
@@ -1,1 +1,35 @@
+import styled from "@emotion/styled";
 
+/* Header Style */
+
+export const HeaderWrapper = styled.div`
+    background: #181818;
+    display: flex;
+    height: 75px;
+    position: absolute;
+    width: 100%;
+`;
+
+export const Button = styled.button`
+  padding: 0;
+  background-color: #181818;
+  font-size: 36px;
+  border: 0;
+  color: #f9f6ee;
+  font-weight: bold;
+  font-family: Lato;
+  font-weight: 700;
+  &:hover {
+    color: #ff4301;
+  }
+  @media (max-width: 850px) {
+    font-size: 24px;
+  }
+`;
+
+export const SubImage = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin: 10px;
+`;
