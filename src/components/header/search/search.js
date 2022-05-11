825b462fa8ab97b47ac8d7881588f8315c3c88b7
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-11 11:20:01 -0400

    created search field and dipsatch changes to value

--- src/components/header/search/search.js
+++ src/components/header/search/search.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { inputSelector, setSearch } from "./searchSlice";
 
 export const Search = () => {
+    const value = useSelector(inputSelector);
+    const activeSub = useSelector(state => state.subreddits.activeSubreddit);
+    const dispatch = useDispatch();
+
+    const onChange = (event) => {
+        dispatch(setSearch(event.target.value))
+    }
 
     return (
         <div>
-            <input></input>
+            <input placeholder={`Search recent posts in ${activeSub}`} onChange={onChange} value={value}></input>
         </div>
     )
 }
