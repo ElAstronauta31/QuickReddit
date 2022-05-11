78cf687d12a7260b7a0f7168bc17070bbbfc5f0e
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2022-05-11 11:14:32 -0400

    added search to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsSlice from "../components/body/posts/postsSlice";
 import subredditSlice from "../components/body/subreddit/subredditSlice";
+import searchSlice from "../components/header/search/searchSlice";
 
 const store = configureStore({
     reducer: {
         subreddits: subredditSlice,
-        posts: postsSlice
+        posts: postsSlice, 
+        search:searchSlice
     }
 })
 
